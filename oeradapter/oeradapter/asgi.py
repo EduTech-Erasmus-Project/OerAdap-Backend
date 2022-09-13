a24f7428969745232aacc032272a9fd689cf8a41
Claudio Mldo <claum0395@gmail.com>
2022-09-13 11:13:25 -0500

    deploy v0.1.0

--- oeradapter/oeradapter/asgi.py
+++ oeradapter/oeradapter/asgi.py
@@ -13,9 +13,8 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
-
 from . import routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oeradapter.settings.prod')
 
 
